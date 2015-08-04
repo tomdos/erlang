f74b38a77a3607337d84b712b9d63cac2b390d6e
Tomas Dosoudil <chimpy@seznam.cz>
2015-08-04 10:05:33 +0100

    phone final state machine finished .. easier solution with only one phone has been chosen :-(

--- erlang-programming/5/exercise_fsm.erl
+++ erlang-programming/5/exercise_fsm.erl
@@ -2,36 +2,58 @@
 -module(exercise_fsm).
 -compile(export_all).
 
-start(MyNumber) -> registe(MyNumber, spawn(exercise_fsm, idle, [])).
+start() -> register(exercise_fsm, spawn(exercise_fsm, idle, [])), ok.
+stop() -> exercise_fsm ! {stop}.
 
-%hang_up()
-%pick_up() ->
-call_number(Number) -> ok.
+%% Controllers
+incomming() -> exercise_fsm ! {incomming}.
+off_hook() -> exercise_fsm ! {off_hook}.
+off_hook(Number) -> exercise_fsm ! {Number, off_hook}.
+on_hook() -> exercise_fsm ! {on_hook}.
+other_on_hook() -> exercise_fsm ! {other_on_hook}.
 
-
-
+%% FSM states
 idle() ->
+  io:format("[idle]~n"),
   receive
-    {Number, incoming} -> start_ringing(), ringing(Number);
-    off_hook -> start_tone(),
-    dial()
+    {incomming} -> start_ringing(), ringing();
+    {Number, off_hook} -> start_tone(), dial(Number);
+    {stop} -> ok
   end.
 
-ringing(Number) ->
+ringing() ->
+  io:format("[ringing]~n"),
   receive
-    {Number, other_on_hook} -> stop_ringing(),
-    idle();
-    {Number, off_hook} ->stop_ringing(),
-    connected(Number)
+    {on_hook} -> stop_ringing(), idle();
+    {other_on_hook} -> busy_tone(), idle();
+    {off_hook} -> stop_ringing(), connected();
+    {stop} -> ok
   end.
 
-dial(Number) -> ok.
+connected() ->
+  io:format("[connected]~n"),
+  receive
+    {on_hook} -> idle();
+    {other_on_hook} -> busy_tone(), idle();
+    {stop} -> ok
+  end.
 
-connected(Number) -> ok.
+dial(Number) ->
+  io:format("[dial]~n"),
+  receive
+    {on_hook} -> idle();
+    {off_hook} -> charge(Number);
+    {other_on_hook} -> busy_tone(), idle();
+    {stop} -> ok
+  end.
 
+charge(Number) ->
+  io:format("[charge]~n"),
+  io:format("Caller of number ~p has been charged. ~n", [Number]),
+  connected().
 
-start_ringing() -> io:format("Phone is ringing.").
-
-start_tone() -> ok.
-
-stop_ringing() -> busy.
+%% Actions output
+start_ringing() -> io:format("Phone is ringing.~n").
+stop_ringing() -> io:format("Phone stopped ringing.~n").
+busy_tone() -> io:format("Phone is busy.~n").
+start_tone() -> io:format("Dialling the number.~n").
